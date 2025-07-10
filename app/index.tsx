f2ead7cca7bc13d9a216e35c28fa2df0e1195d39
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-07-10 17:36:38 +0200

    custom colors

--- app/index.tsx
+++ app/index.tsx
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <SafeAreaView>
       <View className="mx-2.5 mt-5">
-        <Text className="font-work-black text-5xl">Hola mundo!</Text>
-        <Text className="font-work-black text-3xl">Hola mundo!</Text>
-        <Text className="font-work-medium text-2xl">Hola mundo!</Text>
-        <Text className="font-work-light text-xl">Hola mundo!</Text>
+        <Text className="font-work-black text-5xl text-primary">Hola mundo!</Text>
+        <Text className="font-work-black text-3xl text-secondary">Hola mundo!</Text>
+        <Text className="font-work-medium text-2xl text-secondary-100">Hola mundo!</Text>
+        <Text className="font-work-light text-xl text-tertiary">Hola mundo!</Text>
       </View>
     </SafeAreaView>
   );
