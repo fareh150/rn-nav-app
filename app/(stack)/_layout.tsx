f3c954b28e27e7b717f3eaf569cf9e5f8ccb8ed1
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-07-13 12:26:50 +0200

    add global styles to stack screen

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -2,7 +2,15 @@ import { Stack } from 'expo-router'
 
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        //headerShown: false,
+        headerShadowVisible: false,
+        contentStyle: {
+            backgroundColor: 'white',
+        }
+      }}
+    >
         <Stack.Screen
             name="home/index"
             options={{
