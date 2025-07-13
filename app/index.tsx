bfe5907eb9ee24b380124f1f9e6d89a263a6c0be
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-07-13 23:22:46 +0200

    add tabs and usage

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,8 @@ import { Link, Redirect } from 'expo-router';
 import { View, Text, SafeAreaView } from 'react-native';
 
 const App = () => {
-  return <Redirect href="/home" />;
+  //return <Redirect href="/home" />;
+  return <Redirect href="/tabs" />;
   // return (
   //   <SafeAreaView>
   //     <View className="mx-2.5 mt-5">
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react'
+import { Tabs } from 'expo-router';
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: 'Home Screen',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: 'Favorites Screen',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="star-outline" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
+
+export default TabsLayout
