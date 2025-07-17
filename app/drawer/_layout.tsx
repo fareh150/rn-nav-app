fa09354047d86d3463fe0ffcbcf9d037ea7638d8
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-07-18 01:10:47 +0200

    edit drawer, and add user info component

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,13 +1,27 @@
 import { Drawer } from 'expo-router/drawer';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Text } from 'react-native';
+import CustomDrawer from 'components/shared/CustomDrawer';
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
+    <Drawer
+        drawerContent={CustomDrawer}
+        screenOptions={{
+            overlayColor: 'rgba(0, 0, 0, 0.5)',
+            drawerActiveTintColor: 'indigo',
+            drawerContentStyle: { backgroundColor: 'white' },
+            headerShadowVisible: false,
+        }}
+    >
         <Drawer.Screen
           name="user/index" // This is the name of the page and must match the url from root
           options={{
             drawerLabel: 'User',
             title: 'Usuario',
+            drawerIcon: ({ color, size }) => (
+                <Ionicons name="person-circle-outline" size={size} color={color} />
+            )
           }}
         />
         <Drawer.Screen
@@ -15,6 +29,9 @@ const DrawerLayout = () => {
           options={{
             drawerLabel: 'Horario',
             title: 'Horario',
+            drawerIcon: ({ color, size }) => (
+                <Ionicons name="calendar-outline" size={size} color={color} />
+            )
           }}
         />
       </Drawer>
